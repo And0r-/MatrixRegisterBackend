3104ee5f04c9a4917743e8d0c5225ed67fa21204
DESKTOP-E98Q6N0\And0r <ahabegger@gmail.com>
2021-11-29 18:53:30 +0100

    use internal ip

--- app_config.js
+++ app_config.js
@@ -2,7 +2,7 @@ const app_config = {
     matrixBotUser: "SupremeKaos",
     matrixHost: "https://matrix.iot-schweiz.ch/",
 
-    mailHost: "mail.iot-schweiz.ch",
+    mailHost: "192.168.55.9",
     mailPort: 587,
     mailUser: 'cyber@iot-schweiz.ch',
     mailFrom: 'cyber@iot-schweiz.ch',  
